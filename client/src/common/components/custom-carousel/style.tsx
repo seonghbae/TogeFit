83ef342d208a2f22ff1888307df317a182a1d6e5
jinwoo <wwo1278@gmail.com>
2022-07-05 17:30:58 +0900

    [#11] feat: slide 생성

---
+++ client/src/common/components/custom-carousel/style.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  border: 1px solid #000;
+`;
+
+export default Wrapper;
--- client/src/common/components/index.ts
+++ client/src/common/components/index.ts
@@ -1,4 +1,5 @@
-import SearchForm from './SearchForm';
+import CustomCarousel from './custom-carousel/CustomCarousel';
+import SearchForm from './search-form/SearchForm';
 
 // eslint-disable-next-line import/prefer-default-export
-export { SearchForm };
+export { SearchForm, CustomCarousel };
