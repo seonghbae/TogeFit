f7cba75ee366ce1f5707804057197fd8ad39e400
Circlewee <pnj1118@naver.com>
2022-07-05 16:28:13 +0900

    [#9]feat: InfoPage 라우팅

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,4 +1,5 @@
 import Routine from './routine/Routine';
+import InfoPage from './info-page';
 
 // eslint-disable-next-line import/prefer-default-export
-export { Routine };
+export { Routine, InfoPage };
