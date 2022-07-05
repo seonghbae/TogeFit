f83dffb17323309f47f828e1f406e22326e5b219
Circlewee <pnj1118@naver.com>
2022-07-05 14:54:17 +0900

    [#10]feat: theme 정의

---
+++ client/src/styles/Theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  pointColors: {
+    black: '#000',
+    green: '#1A4D2E',
+    orange: '#FF9F29',
+    beige: '#FAF3E3',
+  },
+};
+
+export default theme;
