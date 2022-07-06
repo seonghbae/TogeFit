ec05498b77b2f60ec82c1f0c133cc8c8eff27d21
HyeRyeong <fud3725@naver.com>
2022-07-06 12:49:06 +0900

    [#18]feat: 운동 추가시 예외 사항 추가

--- server/src/services/ExerciseListService.ts
+++ server/src/services/ExerciseListService.ts
@@ -6,6 +6,7 @@ class ExerciseService {
     const foundExercise = await this.exerciseListModel.findByExerciseName(
       exercise
     );
+    return foundExercise;
   }
 
   async getExerciseList() {
@@ -15,6 +16,14 @@ class ExerciseService {
 
   async addExercise(exercise: string) {
     // 검색 코드 추가
+    const foundExercise = await this.exerciseListModel.findByExerciseName(
+      exercise
+    );
+
+    if (foundExercise) {
+      throw new Error('입력하신 운동은 이미 목록에 존재합니다.');
+    }
+
     const newExercise = await this.exerciseListModel.create(exercise);
 
     return newExercise;
