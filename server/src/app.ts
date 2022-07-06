9e061498854ea9066dee1e43706eb2b5cfae056b
Kim Miso <kimms9725@naver.com>
2022-07-06 14:06:54 +0900

    [#16]refactor: 중복 코드 수정

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,6 @@ dbconnect();
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-app.use('/api/user', userRouter);
 
 app.use('/api/user', userRouter);
 app.use('/api/food', foodRouter);
