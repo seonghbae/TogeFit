a1ca6505a5af418a867ab27af39ddc4b038dc594
HyeRyeong <fud3725@naver.com>
2022-07-07 18:25:50 +0900

    [#40]feat: 게시글 수정 API 구현

--- server/src/routers/PostRouter.ts
+++ server/src/routers/PostRouter.ts
@@ -40,4 +40,28 @@ postRouter.delete('/', async (req, res, next) => {
   }
 });
 
+// 게시글 수정
+postRouter.patch('/', async (req, res, next) => {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        'headers의 Content-Type을 application/json으로 설정해주세요.'
+      );
+    }
+    const { postId, contents, meal, routine } = req.body;
+
+    const toUpdateInfo = {
+      ...(contents && { contents }),
+      ...(meal && { meal }),
+      ...(routine && { routine }),
+    };
+
+    console.log(postId, toUpdateInfo);
+    const updatedPost = await postService.updatePost(postId, toUpdateInfo);
+    res.status(201).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
+});
+
 export { postRouter };
