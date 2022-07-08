1400e98f5a7fd0fa608377fc06f59f1e6da1d0db
HyeRyeong <fud3725@naver.com>
2022-07-08 17:38:40 +0900

    [#44] 게시글 추가 API 수정

--- server/src/routers/PostRouter.ts
+++ server/src/routers/PostRouter.ts
@@ -1,25 +1,51 @@
 import { Router } from 'express';
 import is from '@sindresorhus/is';
 import { postService } from '../services';
+import { upload } from '../middlewares/';
+import { getTagList, getPostImageList } from '../utils';
 
 const postRouter = Router();
 
 // 게시글 등록
-postRouter.post('/register', async (req, res, next) => {
-  try {
-    if (is.emptyObject(req.body)) {
-      throw new Error(
-        'headers의 Content-Type을 application/json으로 설정해주세요.'
+postRouter.post(
+  '/register',
+  upload.array('post_image'),
+  async (req, res, next) => {
+    try {
+      if (is.emptyObject(req.body)) {
+        throw new Error(
+          'headers의 Content-Type을 application/json으로 설정해주세요.'
+        );
+      }
+
+      const { userId, contents, tag_list, is_open, meal, routine } = req.body;
+      // 'a,b,c'로 태그를 받아와 배열로 만들어줌
+      const newTagList = getTagList(tag_list);
+
+      // req.files의 location을 받아옴
+      const postImages = getPostImageList(
+        req.files as {
+          [fieldname: string]: Express.Multer.File[];
+        }
       );
+
+      const data = {
+        userId,
+        contents,
+        tag_list: newTagList,
+        post_image: postImages,
+        is_open,
+        meal,
+        routine,
+      };
+      const newPost = await postService.addPost(data);
+
+      res.status(201).json(newPost);
+    } catch (error) {
+      next(error);
     }
-
-    const newPost = await postService.addPost(req.body);
-
-    res.status(201).json(newPost);
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 // 게시글 삭제
 postRouter.delete('/', async (req, res, next) => {
