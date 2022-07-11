f666dbe2c3e0eb8d73e486583fe03c30f3488a42
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-11 18:39:57 +0900

    [#69]feat: CustomCard component 임시 적용

--- client/src/pages/info-page/components/ArticleContainer.tsx
+++ client/src/pages/info-page/components/ArticleContainer.tsx
@@ -1,10 +1,20 @@
+/* eslint-disable no-console */
 import getPath from 'common/utils/getPath';
+import CustomCard from 'common/components/custom-card/CustomCard';
+import { useEffect } from 'react';
+import { customAxios } from 'common/api';
 import * as SC from './ArticleContainerStyle';
-import ExerciseDummyItem from './ExerciseDummyItem';
+// import ExerciseDummyItem from './ExerciseDummyItem';
 import MealDummyItem from './MealDummyItem';
 
 const ArticleContainer = () => {
   const path = getPath();
+
+  useEffect(() => {
+    customAxios.get(`/api/post/user/test123`).then((res) => {
+      console.log(res);
+    });
+  }, []);
 
   return (
     <SC.ContainerSection>
@@ -13,7 +23,7 @@ const ArticleContainer = () => {
         .map(
           () =>
             path === 'exercise' && (
-              <ExerciseDummyItem key={`dummy-item-${Math.random()}`} />
+              <CustomCard key={`dummy-item-${Math.random()}`} />
             )
         )}
       {Array(8)
