34feee8d71a1e1384e188d070bae3948616b7701
jinwoo <wwo1278@gmail.com>
2022-07-11 15:59:00 +0900

    [#58] feat: 운동 추가 모달 컴포넌트 생성

---
+++ client/src/pages/add-routine-page/components/AddExerciseModal.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/no-unstable-nested-components */
+
+const AddExerciseModal = () => (
+  <>
+    <h3>운동 추가</h3>
+    <div>
+      <label htmlFor="name">이름</label>
+      <input type="input" name="name" />
+    </div>
+  </>
+);
+
+export default AddExerciseModal;
