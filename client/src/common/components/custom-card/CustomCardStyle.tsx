b29c6b6e162f41c768bc59586e0a68ea274b01aa
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-12 10:15:29 +0900

    [#69]style: 일부 스타일 조정

--- client/src/common/components/custom-card/CustomCardStyle.tsx
+++ client/src/common/components/custom-card/CustomCardStyle.tsx
@@ -4,38 +4,37 @@ export const Wrapper = styled.div`
   display: flex;
   width: 100%;
   height: 350px;
-  border: 1px solid #000;
-  justify-content: space-evenly;
-  align-items: center;
 `;
 
 export const ArticleImg = styled.img`
-  width: 250px;
-  height: 250px;
+  width: 50%;
+  object-fit: cover;
+  border-radius: 50%;
+  aspect-ratio: 1 / 1;
 `;
 
 export const Article = styled.article`
   display: flex;
-  height: 100%;
+  width: 50%;
+  margin-left: 1rem;
   justify-content: space-around;
   flex-direction: column;
 `;
 
 export const ArticleContent = styled.h1`
-  text-align: center;
   font-size: 2rem;
 `;
 
 export const TagContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(auto-fill, minmax(70px, auto));
   grid-gap: 10px;
 `;
 
 export const Tag = styled.span`
   display: inline-block;
   padding: 5px 15px;
-  border: 1px solid #000;
+  border: 1px solid ${(props) => props.theme.pointColors.lightGreen};
   border-radius: 25px;
   text-align: center;
 `;
--- client/src/pages/info-page/components/ArticleContainerStyle.tsx
+++ client/src/pages/info-page/components/ArticleContainerStyle.tsx
@@ -5,7 +5,7 @@ export const ContainerSection = styled.section`
   background-color: #fff;
   border-radius: 4rem;
   padding: 2rem;
-
+  margin-bottom: 100px;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(420px, 1fr));
   grid-gap: 1rem;
