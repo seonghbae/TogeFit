200570b662d712cd4906311d0eac5c6027589297
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-12 16:23:55 +0900

    [#69]feat: loading 적용

--- client/src/pages/info-page/components/ArticleContainer.tsx
+++ client/src/pages/info-page/components/ArticleContainer.tsx
@@ -18,32 +18,37 @@ const ArticleContainer = () => {
   };
 
   return (
-    <>
-      {isLoading && <div>loading...</div>}
-      <SC.ContainerSection>
-        {articleList.map(
-          (article) =>
-            path === 'exercise' && (
-              <CustomCard
-                key={`custom-card-${Math.random()}`}
-                imgUrl={article.post_image[0]}
-                content={article.contents}
-                tagList={article.tag_list}
-              />
-            )
-        )}
-        {/* TODO: 식사 게시글 처리 */}
-        {Array(8)
-          .fill(0)
-          .map(
-            () =>
-              path === 'meal' && (
-                <MealDummyItem key={`dummy-item-${Math.random()}`} />
+    <SC.ContainerSection>
+      {isLoading ? (
+        <div>loading...</div>
+      ) : (
+        <>
+          {articleList.map(
+            (article) =>
+              path === 'exercise' && (
+                <CustomCard
+                  key={`custom-card-${Math.random()}`}
+                  imgUrl={article.post_image[0]}
+                  content={article.contents}
+                  tagList={article.tag_list}
+                />
               )
           )}
-        {isOpen && <Modal message={errorMessage} handleConfirm={handleClick} />}
-      </SC.ContainerSection>
-    </>
+          {/* TODO: 식사 게시글 처리 */}
+          {Array(8)
+            .fill(0)
+            .map(
+              () =>
+                path === 'meal' && (
+                  <MealDummyItem key={`dummy-item-${Math.random()}`} />
+                )
+            )}
+          {isOpen && (
+            <Modal message={errorMessage} handleConfirm={handleClick} />
+          )}
+        </>
+      )}
+    </SC.ContainerSection>
   );
 };
 
