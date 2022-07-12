c86241fa2720c7d035fccdc76d3e6c96c109699e
HyeRyeong <fud3725@naver.com>
2022-07-12 17:43:39 +0900

    [#80]feat: 잔디 API 구현

--- server/src/services/PostService.ts
+++ server/src/services/PostService.ts
@@ -40,6 +40,11 @@ class PostService {
     return postList;
   }
 
+  async getDateList(userId: string, year: number, month: number) {
+    const dateList = await this.postModel.findByMonth(userId, year, month);
+    return dateList;
+  }
+
   async addPost(postInfo: PostInfo) {
     const createdNewPost = await this.postModel.create(postInfo);
     return createdNewPost;
