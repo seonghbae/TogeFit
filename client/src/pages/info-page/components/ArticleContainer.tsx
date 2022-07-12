8bbb3c9e74858c968e5af2c52efe803874eb9fc6
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-12 15:23:48 +0900

    [#78]feat: 알림 모달 완성

--- client/src/pages/info-page/components/ArticleContainer.tsx
+++ client/src/pages/info-page/components/ArticleContainer.tsx
@@ -1,4 +1,6 @@
 import getPath from 'common/utils/getPath';
+import Modal from 'common/components/alert-modal';
+import { useState } from 'react';
 import * as SC from './ArticleContainerStyle';
 import ExerciseDummyItem from './ExerciseDummyItem';
 import MealDummyItem from './MealDummyItem';
@@ -6,25 +8,44 @@ import MealDummyItem from './MealDummyItem';
 const ArticleContainer = () => {
   const path = getPath();
 
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleConfirm = () => {
+    console.log('confirm');
+  };
+
+  const handleCancel = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <SC.ContainerSection>
-      {Array(8)
-        .fill(0)
-        .map(
-          () =>
-            path === 'exercise' && (
-              <ExerciseDummyItem key={`dummy-item-${Math.random()}`} />
-            )
-        )}
-      {Array(8)
-        .fill(0)
-        .map(
-          () =>
-            path === 'meal' && (
-              <MealDummyItem key={`dummy-item-${Math.random()}`} />
-            )
-        )}
-    </SC.ContainerSection>
+    <>
+      <SC.ContainerSection>
+        {Array(8)
+          .fill(0)
+          .map(
+            () =>
+              path === 'exercise' && (
+                <ExerciseDummyItem key={`dummy-item-${Math.random()}`} />
+              )
+          )}
+        {Array(8)
+          .fill(0)
+          .map(
+            () =>
+              path === 'meal' && (
+                <MealDummyItem key={`dummy-item-${Math.random()}`} />
+              )
+          )}
+      </SC.ContainerSection>
+      {isOpen && (
+        <Modal
+          message="Test Message!"
+          handleConfirm={handleConfirm}
+          handleCancel={handleCancel}
+        />
+      )}
+    </>
   );
 };
 
