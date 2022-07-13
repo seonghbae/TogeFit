64ce77b38fa3932b5894b69000465713482d23cb
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-13 17:15:22 +0900

    [#88]feat: Header의 Link to prop에 location에서 추출한 userId 추가

--- client/src/pages/info-page/components/Header.tsx
+++ client/src/pages/info-page/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import getPath from 'common/utils/getPath';
 import * as SC from './HeaderStyle';
 import ExerciseJandi from './ExerciseJandi';
@@ -6,6 +6,8 @@ import ExerciseJandi from './ExerciseJandi';
 const Header = () => {
   const DEFAULT_PATH = '/info';
   const path = getPath();
+  const location = useLocation();
+  const userId = location.pathname.split('/').at(-1);
 
   return (
     <SC.InfoHeader>
@@ -14,10 +16,10 @@ const Header = () => {
         <SC.NameNavContainer>
           <SC.UserName>나는능이버섯이야</SC.UserName>
           <SC.Nav>
-            <Link to={`${DEFAULT_PATH}/exercise`}>
+            <Link to={`${DEFAULT_PATH}/exercise/${userId}`}>
               <SC.Li active={path === 'exercise'}>운동</SC.Li>
             </Link>
-            <Link to={`${DEFAULT_PATH}/meal`}>
+            <Link to={`${DEFAULT_PATH}/meal/${userId}`}>
               <SC.Li active={path === 'meal'}>식단</SC.Li>
             </Link>
           </SC.Nav>
