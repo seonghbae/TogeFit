af55e5dba38cc68a29f8eeccba6098fe87b26797
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-13 17:55:22 +0900

    [#88]chore: 사용하지 않는 모듈 제거

--- client/src/pages/info-page/hook/useArticle.ts
+++ client/src/pages/info-page/hook/useArticle.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import { customAxios } from 'common/api';
 import { dateObjectAtom } from 'recoil/infoState';
 import { useRecoilValue } from 'recoil';
