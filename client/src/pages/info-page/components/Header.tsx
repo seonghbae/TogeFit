e98e64ebe034706df1fefbde5882ddd97b6bdba6
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-13 17:54:18 +0900

    [#88]feat: 유저 정보를 요청하는 side effect 추가

--- client/src/pages/info-page/components/Header.tsx
+++ client/src/pages/info-page/components/Header.tsx
@@ -1,5 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 import getPath from 'common/utils/getPath';
+import { useEffect, useState } from 'react';
+import { customAxios } from 'common/api';
 import * as SC from './HeaderStyle';
 import ExerciseJandi from './ExerciseJandi';
 
@@ -9,12 +11,25 @@ const Header = () => {
   const location = useLocation();
   const userId = location.pathname.split('/').at(-1);
 
+  const [nickName, setNickName] = useState('');
+  const [profileImg, setProfileImg] = useState('');
+
+  useEffect(() => {
+    async function getRequest() {
+      const response = await customAxios.get(`/api/user/info/${userId}`);
+      setNickName(response.data.nickname);
+      setProfileImg(response.data.profile_image);
+    }
+
+    getRequest();
+  }, [userId]);
+
   return (
     <SC.InfoHeader>
-      <SC.ProfileImg src="/images/user.png" />
+      <SC.ProfileImg src={profileImg} />
       <SC.InfoContainer>
         <SC.NameNavContainer>
-          <SC.UserName>나는능이버섯이야</SC.UserName>
+          <SC.UserName>{nickName}</SC.UserName>
           <SC.Nav>
             <Link to={`${DEFAULT_PATH}/exercise/${userId}`}>
               <SC.Li active={path === 'exercise'}>운동</SC.Li>
