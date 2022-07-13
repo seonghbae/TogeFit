4d5684fdd1d66ae4e3d2299150254ef000eebd96
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-13 14:08:58 +0900

    [#89]feat: 스크롤을 막는 side effect 적용

--- client/src/common/components/loading/index.tsx
+++ client/src/common/components/loading/index.tsx
@@ -6,15 +6,30 @@ interface LoadingProps {
   dotSize?: string;
 }
 
-const Loading = ({ dotColor, dotSize }: LoadingProps) => (
-  <SC.Wrapper>
-    <SC.LoadingBar>
-      <SC.Dot bgColor={dotColor} dotSize={dotSize} />
-      <SC.Dot bgColor={dotColor} dotSize={dotSize} />
-      <SC.Dot bgColor={dotColor} dotSize={dotSize} />
-    </SC.LoadingBar>
-  </SC.Wrapper>
-);
+const Loading = ({ dotColor, dotSize }: LoadingProps) => {
+  useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = '';
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    };
+  }, []);
+
+  return (
+    <SC.Wrapper>
+      <SC.LoadingBar>
+        <SC.Dot bgColor={dotColor} dotSize={dotSize} />
+        <SC.Dot bgColor={dotColor} dotSize={dotSize} />
+        <SC.Dot bgColor={dotColor} dotSize={dotSize} />
+      </SC.LoadingBar>
+    </SC.Wrapper>
+  );
+};
 
 Loading.defaultProps = {
   dotColor: '#fff',
