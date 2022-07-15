3e950042f687cf82528cbebd102d2a0d4e280973
seonghbae <fullbloomh3ck@gmail.com>
2022-07-15 09:00:44 +0900

    [#73]feat: 버튼 컴포넌트 수정

--- client/src/pages/diet-page/components/DietInfo.tsx
+++ client/src/pages/diet-page/components/DietInfo.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { MouseEventHandler, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import Meal from './Meal';
 import dietState from '../states/dietState';
@@ -7,22 +7,27 @@ import useFood from '../hooks/useFood';
 import * as SC from './DietInfoStyle';
 
 const DietInfo = () => {
+  const { food, getFood } = useFood();
   const diet = useRecoilValue(dietState);
   const date = new Date(diet.createdAt);
-  const { food, getFood } = useFood();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getFood();
   }, []);
 
+  const handleAddMeal: MouseEventHandler<HTMLButtonElement> = () => {
+    navigate('/diet/add');
+  };
+
   return (
     <SC.DietInfoContainer>
       <div>{`${date.getMonth() + 1}/${date.getDate()}`}</div>
-      <Link to="/diet/add">
-        <SC.ButtonWrapper>
-          <button type="button">+</button>
-        </SC.ButtonWrapper>
-      </Link>
+      <SC.ButtonWrapper>
+        <button type="button" onClick={handleAddMeal}>
+          +
+        </button>
+      </SC.ButtonWrapper>
       {food?.status === 200 &&
         diet.meals.map((meal, index) => (
           <Meal
