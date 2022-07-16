7e9a4f0709701021c2d1638038187389332d4c8b
HyeRyeong <fud3725@naver.com>
2022-07-16 16:25:03 +0900

    [#105]feat: 게시글 테스트 코드 - 실패 케이스 추가

--- server/tests/post.test.ts
+++ server/tests/post.test.ts
@@ -38,7 +38,7 @@ describe('게시글 불러오기 TEST', () => {
   test('전체 게시글 불러오기 성공', async () => {
     const postList = await postService.getAllPost();
     expect(Array.isArray(postList)).toBe(true);
-    expect(posts.length).toBe(10);
+    expect(posts.length).toBe(postList.length);
   });
 
   test('ObjectId로 한 개의 게시글 불러오기 성공', async () => {
@@ -61,7 +61,10 @@ describe('게시글 불러오기 TEST', () => {
 
   test('게시글 가져오기(페이지네이션) 성공', async () => {
     const userId = 'test';
-    const date = { year: '2022', month: 7 };
+    const date = {
+      year: new Date().getFullYear().toString(),
+      month: Number(new Date().getMonth()) + 1,
+    };
     const conditions = { limit: 5, reqNumber: 0 };
     const postList = await postService.getPostListByDate(
       userId,
@@ -71,7 +74,16 @@ describe('게시글 불러오기 TEST', () => {
     expect(postList.length).toBe(5);
   });
 
-  test('유저 ID, 연도, 월별로 게시글 쓴 날짜 가져오기 성공', async () => {
+  test('게시글 가져오기(페이지네이션) 실패 - 월의 범위(1~12)를 벗어난 경우', async () => {
+    const userId = 'test';
+    const date = { year: '2022', month: 17 };
+    const conditions = { limit: 5, reqNumber: 0 };
+    await expect(
+      postService.getPostListByDate(userId, date, conditions)
+    ).rejects.toThrow();
+  });
+
+  test('게시글 쓴 날짜 가져오기(잔디 API) 성공', async () => {
     const userId = 'test';
     const year = Number(new Date().getFullYear());
     const month = Number(new Date().getMonth()) + 1;
@@ -79,6 +91,14 @@ describe('게시글 불러오기 TEST', () => {
     const postList = await postService.getDateList(userId, year, month);
     expect(Array.isArray(postList)).toBe(true);
     expect(postList).toContain(Number(new Date().getDate()));
+  });
+
+  test('게시글 쓴 날짜 가져오기(잔디 API) 실패 - 월의 범위(1~12)를 벗어나는 경우', async () => {
+    const userId = 'test';
+    const year = Number(new Date().getFullYear());
+    const month = 18;
+    const postList = await postService.getDateList(userId, year, month);
+    expect(postList.length).toBe(0);
   });
 });
 
@@ -90,6 +110,15 @@ describe('게시글 추가 TEST', () => {
     });
     expect(typeof post).toBe('object');
     expect(post.userId).toEqual('test123');
+  });
+
+  test('게시글 추가 실패 - 잘못된 아이디 형식 제공', async () => {
+    await expect(
+      postService.addPost({
+        userId: '',
+        contents: 'test contents',
+      })
+    ).rejects.toThrow();
   });
 });
 
