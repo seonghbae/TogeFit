277035143b3649d7c8b12e371d354dcf021ab5c1
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-19 00:00:27 +0900

    [#127]feat: 이미 로그인 되어있다면 /으로 navigate시키는 side effect 추가

--- client/src/pages/login-page/components/LoginForm.tsx
+++ client/src/pages/login-page/components/LoginForm.tsx
@@ -1,12 +1,16 @@
 import { FormInputType } from 'types/interfaces';
 import { AlertModal } from 'common/components';
 import Loading from 'common/components/loading';
+import { useEffect } from 'react';
+import { getUserId } from 'common/utils/getUserId';
+import { useNavigate } from 'react-router-dom';
 import CustomForm from './CustomForm';
 import { LoginButton } from './LoginFormStyle';
 import useLogin from '../hook/useLogin';
 
 const LoginForm = () => {
   const { asyncLogin, error, isLoading, isError, setIsError } = useLogin();
+  const navigate = useNavigate();
 
   const onSubmit = async (data: FormInputType) => {
     asyncLogin(data);
@@ -15,6 +19,10 @@ const LoginForm = () => {
   const handleConfirm = () => {
     setIsError(false);
   };
+
+  useEffect(() => {
+    if (getUserId()) navigate('/');
+  }, []);
 
   return (
     <>
