953690305c645c2adfd2d1c9bddde76b502f54ac
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-19 16:17:43 +0900

    [#134]feat: ArticleModal 디자인 완성

--- client/src/common/components/article-modal/index.tsx
+++ client/src/common/components/article-modal/index.tsx
@@ -1,9 +1,44 @@
+import { PostResponse } from 'types/interfaces';
 import * as SC from './style';
 
-const ArticleModal = () => {
+interface ArticleProps {
+  post: PostResponse | undefined;
+}
+
+const ArticleModal = ({ post }: ArticleProps) => {
   const a = 10;
 
-  return <SC.Wrapper>article modal!</SC.Wrapper>;
+  return (
+    <SC.Wrapper>
+      {!post ? (
+        <SC.Modal>게시글이 존재하지 않습니다!</SC.Modal>
+      ) : (
+        <SC.Modal>
+          <SC.CloseIcon />
+          <SC.ArticleImg src="https://team-16-s3.s3.ap-northeast-2.amazonaws.com/mREP5nAR9.jpeg" />
+          <SC.Article>
+            <SC.ArticleContent>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius sed
+              non, dolores similique corporis a id perspiciatis iure obcaecati
+              atque ducimus corrupti, facilis, iste sunt ratione optio molestias
+              aliquid consequuntur.
+            </SC.ArticleContent>
+            <SC.TagContainer>
+              <SC.Tag>#lorem10</SC.Tag>
+              <SC.Tag>#lorem11</SC.Tag>
+              <SC.Tag>#lorem12</SC.Tag>
+            </SC.TagContainer>
+            <SC.CommentContainer>
+              <h3>Comments</h3>
+              <li>안녕하세요!</li>
+              <li>안녕하세요!</li>
+              <li>안녕하세요!</li>
+            </SC.CommentContainer>
+          </SC.Article>
+        </SC.Modal>
+      )}
+    </SC.Wrapper>
+  );
 };
 
 export default ArticleModal;
--- client/src/common/components/article-modal/style.tsx
+++ client/src/common/components/article-modal/style.tsx
@@ -1,3 +1,93 @@
 import styled from 'styled-components';
+import { Close } from 'styled-icons/material-rounded';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 9998;
+`;
+
+export const Modal = styled.div`
+  display: flex;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 60%;
+  max-width: 1000px;
+  height: 70%;
+  padding: 1rem;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  z-index: 9999;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    height: auto;
+    padding: 3rem 1rem;
+  }
+`;
+
+export const CloseIcon = styled(Close)`
+  position: absolute;
+  top: 3%;
+  right: 3%;
+  width: 1.8rem;
+  cursor: pointer;
+`;
+
+export const ArticleImg = styled.img`
+  width: 50%;
+  object-fit: cover;
+  aspect-ratio: 1 / 1;
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    padding-top: 1rem;
+  }
+`;
+
+export const Article = styled.article`
+  display: flex;
+  width: 50%;
+  margin-left: 1rem;
+  justify-content: center;
+  flex-direction: column;
+
+  & > div + div {
+    margin-top: 2rem;
+  }
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    margin: 0;
+  }
+`;
+
+export const ArticleContent = styled.div`
+  font-size: 1.2rem;
+  line-height: 140%;
+`;
+
+export const CommentContainer = styled.div`
+  & > li + li {
+    margin-top: 10px;
+  }
+`;
+
+export const TagContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(90px, auto));
+  grid-gap: 10px;
+`;
+
+export const Tag = styled.span`
+  display: inline-block;
+  padding: 5px 15px;
+  border: 1px solid ${(props) => props.theme.pointColors.lightGreen};
+  border-radius: 25px;
+  text-align: center;
+`;
