9fc413152ad12bfbea1caeab82ea9b633db0aa6e
jinwoo <wwo1278@gmail.com>
2022-07-19 16:26:01 +0900

    [#137] feat: 가입

--- client/src/pages/login-page/LoginPage.tsx
+++ client/src/pages/login-page/LoginPage.tsx
@@ -5,18 +5,18 @@ import { customAxios } from 'common/api';
 
 import LoginForm from './components/LoginForm';
 
-import { Wrapper, RegisterLink } from './LoginPageStyle';
+import * as SC from './LoginPageStyle';
 
 const LoginPage: React.FC = () => (
-  <Wrapper>
+  <SC.Wrapper>
     <div>
       <LoginForm />
       <p>
         <span>혹시 회원이 아니신가요?</span>
-        <RegisterLink to="/register">회원가입</RegisterLink>
+        <SC.RegisterLink to="/register">회원가입</SC.RegisterLink>
       </p>
     </div>
-  </Wrapper>
+  </SC.Wrapper>
 );
 
 export default LoginPage;
