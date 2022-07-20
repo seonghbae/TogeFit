ae67a8211912e00245db9e0b17c076c89e119ba6
jinwoo <wwo1278@gmail.com>
2022-07-21 00:29:35 +0900

    [#160] fix: 내 게시물 스타일

--- client/src/pages/info-page/components/ExerciseJandi.tsx
+++ client/src/pages/info-page/components/ExerciseJandi.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable no-nested-ternary */
 import getPadString from 'common/utils/getPadString';
 import {
   KeyboardArrowLeft,
   KeyboardArrowRight,
 } from 'styled-icons/material-outlined';
+import { Dumbbell as NoDumbbell } from 'styled-icons/fluentui-system-regular';
+import { Dumbbell as YesDumbbell } from 'styled-icons/fluentui-system-filled';
 import * as SC from './ExerciseJandiStyle';
 import useJandi from '../hook/useJandi';
 
@@ -27,9 +30,15 @@ const ExerciseJandi = () => {
       <SC.DaySpan>Sun</SC.DaySpan>
       <SC.DaySpan sat>Sat</SC.DaySpan>
       <SC.JandiContainer>
+        {/* <SC.JandiItem key={Math.random()} active={jandi.isActive} /> */}
+
         {jandiList.map((jandi) =>
           jandi.isNow ? (
-            <SC.JandiItem key={Math.random()} active={jandi.isActive} />
+            jandi.isActive ? (
+              <YesDumbbell size="30px" color="green" />
+            ) : (
+              <NoDumbbell size="30px" color="" />
+            )
           ) : (
             <div key={Math.random()} />
           )
