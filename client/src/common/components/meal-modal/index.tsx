b4d9938dccc94819fc452570173a92acccea7df2
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-20 18:58:28 +0900

    [#144]feat: 데이터 가공 후 출력

--- client/src/common/components/meal-modal/index.tsx
+++ client/src/common/components/meal-modal/index.tsx
@@ -1,16 +1,30 @@
+import { nanoid } from 'nanoid';
 import { IDiet } from 'types/interfaces';
 import * as SC from './style';
 
 interface MealModalProps {
-  post: IDiet | undefined;
+  post: IDiet;
 }
 
 const MealModal = ({ post }: MealModalProps) => {
-  console.log(post);
+  const { meals } = post;
 
   return (
     <SC.Wrapper>
-      <SC.Modal>MealModal!</SC.Modal>
+      <SC.Modal>
+        <SC.CloseIcon />
+        {meals.map((mealList, index) => (
+          <SC.MealContainer key={nanoid()}>
+            <SC.MealName>{`식사 ${index + 1}`}</SC.MealName>
+            {mealList.meal_list.map((meal) => (
+              <SC.FoodContainer key={nanoid()}>
+                <SC.FoodName>{meal.foodName}</SC.FoodName>
+                <SC.FoodQuantity>{`${meal.quantity}g`}</SC.FoodQuantity>
+              </SC.FoodContainer>
+            ))}
+          </SC.MealContainer>
+        ))}
+      </SC.Modal>
     </SC.Wrapper>
   );
 };
--- client/src/common/components/meal-modal/style.tsx
+++ client/src/common/components/meal-modal/style.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Close } from 'styled-icons/material-rounded';
 
 export const Wrapper = styled.div`
   position: fixed;
@@ -15,16 +16,51 @@ export const Modal = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
-  width: 60%;
-  max-width: 1200px;
+  width: 40%;
+  max-width: 800px;
   padding: 4rem 2rem;
   transform: translate(-50%, -50%);
   background-color: #fff;
   z-index: 9999;
+  flex-direction: column;
 
   @media only screen and (max-width: 768px) {
-    flex-direction: column;
     height: auto;
     padding: 3rem 1rem;
   }
+`;
+
+export const CloseIcon = styled(Close)`
+  position: absolute;
+  top: 3%;
+  right: 3%;
+  width: 1.8rem;
+  cursor: pointer;
+`;
+
+export const MealContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  & + & {
+    margin-top: 2rem;
+  }
+`;
+
+export const MealName = styled.h3`
+  margin: 0;
+  padding-bottom: 5px;
+  border-bottom: 1px solid ${(props) => props.theme.pointColors.orange};
+`;
+
+export const FoodContainer = styled.div`
+  margin-left: 10px;
+`;
+
+export const FoodName = styled.h4`
+  margin: 0;
+`;
+
+export const FoodQuantity = styled.p`
+  margin: 0;
 `;
