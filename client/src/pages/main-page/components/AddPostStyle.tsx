19b1d4ab47b14e6adc0d8d65b7c31ace31be407f
seonghbae <fullbloomh3ck@gmail.com>
2022-07-20 18:20:34 +0900

    [#142]feat: API랑 연결

---
+++ client/src/pages/main-page/components/AddPostStyle.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const AddPostContainer = styled.div`
+  border: 1px solid;
+  background-color: #fff;
+  margin: 0 5%;
+  height: 100vh;
+  width: 90% input, textarea {
+    box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+  }
+`;
+
+export const FormWrapper = styled.form`
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 60%;
+  width: 100%;
+  row-gap: 1.5rem;
+
+  h2 {
+    margin-top: 25rem;
+    margin-bottom: 5rem;
+  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 43%;
+
+  textarea {
+    border: none;
+    resize: none;
+  }
+`;
+
+export const RadioBoxContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
