dccb98f7ad058bd6ad806b1b22f7158327f2edef
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-21 18:54:03 +0900

    [#161]style: 모달 스타일 완성

--- client/src/pages/info-page/components/DateInfoStyle.tsx
+++ client/src/pages/info-page/components/DateInfoStyle.tsx
@@ -17,4 +17,37 @@ export const Title = styled.h3<{ fontSize?: number }>`
     cursor: pointer;
     vertical-align: middle;
   }
+
+  > button {
+    font-size: 1.2rem;
+  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  margin: 20px 0;
+`;
+
+export const DateInput = styled.input`
+  width: 80px;
+  font-size: 1.1rem;
+  text-align: center;
+  border-bottom: 3px solid gray;
+  padding: 5px 10px;
+
+  &[type='number']::-webkit-inner-spin-button,
+  &[type='number']::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  & + & {
+    margin-left: 20px;
+  }
+
+  :focus {
+    border-bottom: 3px solid ${(props) => props.theme.pointColors.orange};
+  }
 `;
