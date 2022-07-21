f3228aa7411cf1639f3b42fb43121dbcde62f522
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-22 01:45:16 +0900

    [#178]feat: 음식 폼에서 필수 값들이 입력되지 않으면 요청을 보낼 수 없게 설정

--- client/src/pages/diet-page/components/AddFoodModal.tsx
+++ client/src/pages/diet-page/components/AddFoodModal.tsx
@@ -5,6 +5,7 @@
 
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { useRecoilState } from 'recoil';
+import { AlertModal } from 'common/components';
 import { IFood } from 'types/interfaces';
 import foodListState from '../states/foodListState';
 import useFoodAdd from '../hooks/useFoodAdd';
@@ -18,13 +19,28 @@ interface IProps {
 }
 
 const AddFoodModal = ({ isOpen, setIsOpen, isCancel, setIsCancel }: IProps) => {
-  const { register, handleSubmit, resetField } = useForm<IFood>();
-  const { addFood } = useFoodAdd();
+  const {
+    register,
+    handleSubmit,
+    resetField,
+    formState: { errors },
+  } = useForm<IFood>({ mode: 'onChange' });
+  const { showError, message, addFood, setShowError } = useFoodAdd();
   const [food, setFood] = useRecoilState(foodListState);
+
+  const reset = () => {
+    resetField('name');
+    resetField('carbohydrate');
+    resetField('protein');
+    resetField('fat');
+    resetField('quantity');
+    resetField('calories');
+  };
 
   const handleCancel = () => {
     setIsOpen(false);
     setIsCancel(true);
+    reset();
   };
 
   const handleDivClick = (e: React.MouseEvent<HTMLElement>) => {
@@ -34,51 +50,78 @@ const AddFoodModal = ({ isOpen, setIsOpen, isCancel, setIsCancel }: IProps) => {
   const onSubmit: SubmitHandler<IFood> = (data) => {
     addFood(data);
     setFood((cur) => [...cur, data.name]);
-    resetField('name');
-    resetField('carbohydrate');
-    resetField('protein');
-    resetField('fat');
-    resetField('quantity');
-    resetField('calories');
+    reset();
     setIsOpen(false);
   };
 
+  const handleConfirm = () => {
+    setShowError(false);
+  };
+
   return (
-    <SC.FoodContainer view={isOpen} onClick={handleCancel}>
-      <form onSubmit={handleSubmit(onSubmit)} onClick={handleDivClick}>
-        <h3>음식 추가</h3>
-        <div>
-          <label htmlFor="name">이름 &nbsp; &nbsp; &nbsp;</label>
-          <input type="text" {...register('name')} />
-        </div>
-        <div>
-          <label htmlFor="carbohydrate">탄수화물</label>
-          <input type="text" {...register('carbohydrate')} />
-        </div>
-        <div>
-          <label htmlFor="protein">단백질 &nbsp;&nbsp;</label>
-          <input type="text" {...register('protein')} />
-        </div>
-        <div>
-          <label htmlFor="fat">지방 &nbsp; &nbsp; &nbsp;</label>
-          <input type="text" {...register('fat')} />
-        </div>
-        <div>
-          <label htmlFor="quantity">양 &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</label>
-          <input type="text" {...register('quantity')} />
-        </div>
-        <div>
-          <label htmlFor="calories">칼로리 &nbsp;&nbsp;</label>
-          <input type="text" {...register('calories')} />
-        </div>
-        <div>
-          <button type="submit">확인</button>
-          <button type="button" onClick={handleCancel}>
-            취소
-          </button>
-        </div>
-      </form>
-    </SC.FoodContainer>
+    <>
+      <SC.FoodContainer view={isOpen} onClick={handleCancel}>
+        <form onSubmit={handleSubmit(onSubmit)} onClick={handleDivClick}>
+          <h3>음식 추가</h3>
+          <div>
+            <label htmlFor="name">이름 &nbsp; &nbsp; &nbsp;</label>
+            <input type="text" {...register('name', { required: true })} />
+            {errors.name && errors.name.type === 'required' && (
+              <p>이름을 입력해주세요.</p>
+            )}
+          </div>
+          <div>
+            <label htmlFor="carbohydrate">탄수화물</label>
+            <input
+              type="text"
+              {...register('carbohydrate', { required: true })}
+            />
+            {errors.carbohydrate && errors.carbohydrate.type === 'required' && (
+              <p>탄수화물의 양을 입력해주세요.</p>
+            )}
+          </div>
+          <div>
+            <label htmlFor="protein">단백질 &nbsp;&nbsp;</label>
+            <input type="text" {...register('protein', { required: true })} />
+            {errors.protein && errors.protein.type === 'required' && (
+              <p>단백질의 양을 입력해주세요.</p>
+            )}
+          </div>
+          <div>
+            <label htmlFor="fat">지방 &nbsp; &nbsp; &nbsp;</label>
+            <input type="text" {...register('fat', { required: true })} />
+            {errors.fat && errors.fat.type === 'required' && (
+              <p>지방의 양을 입력해주세요.</p>
+            )}
+          </div>
+          <div>
+            <label htmlFor="quantity">
+              양 &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;
+            </label>
+            <input type="text" {...register('quantity', { required: true })} />
+            {errors.quantity && errors.quantity.type === 'required' && (
+              <p>단위 무게를 입력해주세요.</p>
+            )}
+          </div>
+          <div>
+            <label htmlFor="calories">칼로리 &nbsp;&nbsp;</label>
+            <input type="text" {...register('calories', { required: true })} />
+            {errors.calories && errors.calories.type === 'required' && (
+              <p>칼로리를 입력해주세요.</p>
+            )}
+          </div>
+          <div>
+            <button type="submit">확인</button>
+            <button type="button" onClick={handleCancel}>
+              취소
+            </button>
+          </div>
+        </form>
+      </SC.FoodContainer>
+      {showError && (
+        <AlertModal message={message} handleConfirm={handleConfirm} />
+      )}
+    </>
   );
 };
 
