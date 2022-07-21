74437df2ec890334fe56f8366c2da768b360b9d2
[SW2/서울/2] 박노준 <pnj1118@naver.com>
2022-07-21 18:42:12 +0900

    [#161]feat: 년월 이동 모달 구현

--- client/src/pages/info-page/components/DateInfo.tsx
+++ client/src/pages/info-page/components/DateInfo.tsx
@@ -2,30 +2,74 @@ import {
   KeyboardArrowLeft,
   KeyboardArrowRight,
 } from 'styled-icons/material-outlined';
+import { useState, useRef, MutableRefObject } from 'react';
+
 import { DateObject } from 'recoil/infoState';
 import getPadString from 'common/utils/getPadString';
+import { AlertModal } from 'common/components';
+import { actionType } from '../hook/useJandi';
 import * as SC from './DateInfoStyle';
 
 interface DateProps {
   dateObject: DateObject;
   fontSize?: number;
-  changeDate: (type: string) => void;
+  changeDate: (type: actionType, goTo?: DateObject | undefined) => void;
 }
 
-const DateInfo = ({ dateObject, fontSize, changeDate }: DateProps) => (
-  <SC.Title fontSize={fontSize}>
-    <KeyboardArrowLeft
-      onClick={() => {
-        changeDate('left');
-      }}
-    />
-    {`${dateObject.year}.${getPadString(dateObject.month + 1, 2)}`}
-    <KeyboardArrowRight
-      onClick={() => {
-        changeDate('right');
-      }}
-    />
-  </SC.Title>
-);
+const DateInfo = ({ dateObject, fontSize, changeDate }: DateProps) => {
+  const [isOpen, setOpen] = useState(false);
+  const yearRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const monthRef = useRef() as MutableRefObject<HTMLInputElement>;
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleConfirm = () => {
+    changeDate('jump', {
+      year: parseInt(yearRef.current.value, 10),
+      month: parseInt(monthRef.current.value, 10) - 1,
+    });
+    handleCancel();
+  };
+
+  return (
+    <>
+      <SC.Title fontSize={fontSize}>
+        <KeyboardArrowLeft
+          onClick={() => {
+            changeDate('left');
+          }}
+        />
+        <button type="button" onClick={handleOpen}>
+          {`${dateObject.year}.${getPadString(dateObject.month + 1, 2)}`}
+        </button>
+        <KeyboardArrowRight
+          onClick={() => {
+            changeDate('right');
+          }}
+        />
+      </SC.Title>
+      {isOpen && (
+        <AlertModal
+          message="이동할 년월을 입력하세요."
+          handleConfirm={handleConfirm}
+          handleCancel={handleCancel}
+        >
+          <input type="text" defaultValue={dateObject.year} ref={yearRef} />
+          <input
+            type="text"
+            defaultValue={dateObject.month + 1}
+            ref={monthRef}
+          />
+        </AlertModal>
+      )}
+    </>
+  );
+};
 
 export default DateInfo;
