838d3faf1d59ccf86aa45312ca4a9d7c0c9282f7
jinwoo <wwo1278@gmail.com>
2022-07-22 13:58:40 +0900

    [#186] fix: 식단 스타일

--- client/src/pages/diet-page/components/DietListStyle.tsx
+++ client/src/pages/diet-page/components/DietListStyle.tsx
@@ -9,11 +9,11 @@ export const DietListContainer = styled.div`
 export const Header = styled.div`
   display: flex;
   justify-content: space-between;
-  margin: 3%;
+  margin: 5%;
   padding: 3% 0;
   > button {
     box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
-    margin-right: 1rem;
+
     padding: 5px 10px;
     font-size: 1.2rem;
     border-radius: 1rem;
